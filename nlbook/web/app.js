10e0d377c80337b283520e7f8602fd9ea5d39127
RodrigoAldana <rodrigo.aldana.lopez@gmail.com>
2026-03-13 09:45:40 +0100

    slider added to nlbook

--- nlbook/web/app.js
+++ nlbook/web/app.js
@@ -1,5 +1,42 @@
 import { openPanel, setPanelTag, clearPanelUI } from "./panel_runtime.js";
 
+
+const appLayout = document.querySelector(".app")
+const resizer = document.getElementById("resizer")
+
+let dragging = false
+
+resizer.addEventListener("mousedown",(e)=>{
+  dragging = true
+  document.body.style.userSelect = "none"
+  e.preventDefault()
+})
+
+window.addEventListener("mouseup", ()=>{
+  dragging = false
+  document.body.style.userSelect = ""
+})
+
+window.addEventListener("mousemove",(e)=>{
+
+  if(!dragging) return
+
+  const rect = appLayout.getBoundingClientRect()
+
+  const x = e.clientX - rect.left
+  const width = rect.width
+
+  let left = x/width*100
+
+  if(left < 15) left = 15
+  if(left > 85) left = 85
+
+  const right = 100-left
+
+  appLayout.style.gridTemplateColumns =
+    left + "% 6px " + right + "%"
+})
+
 const sourceEl = document.getElementById("source");
 
 async function typeset(root){
